d9f06bed24560c632700e4ae20d27a553bc49c79
Sean Dutton-Jones <seanduttonjones@gmail.com>
2022-09-13 00:55:51 -0700

    Add TagIndex

---
+++ TagIndex.py
@@ -0,0 +1,31 @@
+class TagIndex:
+
+    def __init__(self) -> None:
+        self.index = {}
+        self.inverse_index = {}
+
+    def add_value(self, tag, value):
+        if(tag not in self.index):
+            self.index[tag] = []
+
+        self.index[tag].append(value)
+
+        if(value not in self.inverse_index):
+            self.inverse_index[value] = []
+        
+        self.inverse_index[value].append(tag)
+
+    def remove_value(self, value):
+        tags = self.inverse_index.pop(value, None)
+
+        for tag in tags:
+            self.index[tag].remove(value)
+            
+            if(len(self.index[tag]) == 0):
+                self.index.pop(tag, None)
+
+    def find(self, tag):
+        if(tag not in self.index):
+            return None
+
+        return self.index[tag]
--- taggert.py
+++ taggert.py
@@ -1,1 +1,21 @@
-print("Hello World!")
+import click
+
+from TagIndex import TagIndex
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.argument("tags", nargs=-1)
+def search(tags):
+    print("Search")
+    pass
+
+@cli.command()
+def list():
+    print("List")
+    pass
+
+if __name__ == "__main__":
+    cli()
