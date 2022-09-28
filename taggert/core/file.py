5d19b1ea3484471322afdd903a31749e15ad815c
seanduttonjones <sean4424@me.com>
2022-09-28 16:45:29 -0700

    Added File.py The File object represents an OS type file. Currently only a template and has NOT been implemented yet.

---
+++ taggert/core/file.py
@@ -0,0 +1,49 @@
+class File:
+
+    def __init__(self, pathname: str):
+        pass
+
+    def get_name(self):
+        pass
+
+    def get_extension(self):
+        pass
+
+    def get_parent(self):
+        pass
+
+    def get_path(self):
+        pass
+
+    def get_size(self):
+        pass
+
+    def exists(self):
+        pass
+
+    def hash(self):
+        pass
+
+    def is_directory(self):
+        pass
+
+    def is_file(self):
+        pass
+
+    def is_hidden(self):
+        pass
+
+    def last_modified(self):
+        pass
+
+    def list_files(self):
+        pass
+
+    # def list_files(file_filter: FileFilter):
+    #     pass
+
+    def list_directories(self):
+        pass
+
+    # def list_directories(file_filter: FileFilter):
+    #     pass
